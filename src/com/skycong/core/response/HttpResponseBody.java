a580d48fa56d0c69e2a52b2990cc29185176f349
ruanmingcong <ruanmingcong@qq.com>
2019-07-06 21:01:18 +0800

    request and response

---
+++ src/com/skycong/core/response/HttpResponseBody.java
@@ -0,0 +1,7 @@
+package com.skycong.core.response;
+
+/**
+ * @author RMC 2019/7/6 19:48
+ */
+public class HttpResponseBody {
+}
---
+++ src/com/skycong/core/response/HttpResponseHeader.java
@@ -0,0 +1,7 @@
+package com.skycong.core.response;
+
+/**
+ * @author RMC 2019/7/6 19:45
+ */
+public class HttpResponseHeader {
+}
